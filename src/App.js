65e4780dd49fbf9a8f4c9e24ff0a39d75f8db1d7
AdminNormTherapy <111477333+AdminNormTherapy@users.noreply.github.com>
2022-10-23 08:08:35 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,20 +13,23 @@ import {
 4. Open the Developer Console to see speech segement outputs.
 5. Start developing with Speechly (see https://docs.speechly.com/quick-start/)
 */
+const localTime = localstorage.getItem("SpeechlyFirstConnect");
 
 function App() {
   const { segment } = useSpeechContext()
-  localStorage.setItem('speechly_sentence.json', JSON.stringify({}));
+  localStorage.setItem('speechly_sentence.json', JSON.stringify([]));
   useEffect(() => {
     if (segment) {
       const plainString = segment.words.filter(w => w.value).map(w => w.value).join(' ');
       console.log(plainString);
       const cur1 = segment.words[segment.words.length-1];
+      cur1.startTimestamp = cur1.startTimestamp + this.localTime;
+      cur1.endTimestamp = cur1.endTimestamp + this.localTime;
       localStorage.setItem('speechly_current.json', JSON.stringify(cur1));
       const saved1 = localStorage.getItem("speechly_sentence.json");
       const saved2 = JSON.parse(saved1);
-      const saved3 = {...saved2, ...cur1};
-      localStorage.setItem('speechly_sentence.json', JSON.stringify(saved3));
+      saved2.push(cur1);
+      localStorage.setItem('speechly_sentence.json', JSON.stringify(saved2));
       if (segment.isFinal) {
         console.log("✅", plainString);
       }
